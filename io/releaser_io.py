0e7f931fb4773dc5ab68cb5f1ed31cb0fc846760
Nerijus <dnerijus72@gmail.com>
2023-10-08 15:30:09 +0300

    lcd scrolling text

--- io/releaser_io.py
+++ io/releaser_io.py
@@ -14,7 +14,7 @@ if dummy_io == False:
   from gpiozero import Button
   from gpiozero import LED
   from RPLCD.i2c import CharLCD
-  lcd = CharLCD('PCF8574', 0x27)
+  lcd = CharLCD('PCF8574', 0x27, auto_linebreaks=False)
   button_led = LED(pinMap['button_led'])
   button = Button(pinMap['release'])
   ioMap = {
@@ -75,15 +75,41 @@ def led_blink(id, onoff):
     thread = None
     ioMap[id].off()
 
-def lcd_write(text):
+lcd_thread = None
+
+def _lcd_write(line, line_num):
+  s = line + '   '
+
+  while True:
+    for i in range(len(s) - 16 + 1):
+      lcd.cursor_pos = (line_num, 0)
+      lcd.write_string(s[i:i+16])
+      sleep(0.5)
+
+def lcd_write(lines):
+  global lcd_thread
+
   if dummy_io:
     return
 
-  lcd.clear()
-  lcd.write_string(text)
+  lcd_clear()
+
+  for i in range(len(lines)):
+    if len(lines[i]) > 16:
+      lcd_thread = multiprocessing.Process(target=_lcd_write, args=(lines[i],i))
+      lcd_thread.start()
+    else:
+      lcd.cursor_pos = (i, 0)
+      lcd.write_string(lines[i])
 
 def lcd_clear():
+  global lcd_thread
+
   if dummy_io:
     return
+
+  if lcd_thread != None:
+    lcd_thread.terminate()
+    lcd_thread = None
 
   lcd.clear()
